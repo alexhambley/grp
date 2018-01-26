a0473d0cf0acb396caa9801a1d513822a6cd440f
alexhambley <hambleyjones@gmail.com>
2018-01-26 14:56:56 +0000

    allthemes now shows all themes, roles and elements

--- src/allThemes.php
+++ src/allThemes.php
@@ -16,52 +16,109 @@
                 <a href="#"> Students </a>
             </li>
             <li class="active">
-                <a href="newThemes.php"> View All Themes </a>
+                <a href="allThemes.php"> View All Themes </a>
             </li>
             <li>
-                <a href="#"> Admin </a>
+                <a href="login.php"> Admin </a>
             </li>
         </ul>
     </nav>
 
-<!-- Load in all the themes -->
+<!-- all the THEMES -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">
+                    #
+                </th>
+                <th scope="col">
+                    Theme Name
+                </th>
+                <th scope="col">
+                    See More
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            $stmt = $conn->prepare("SELECT id, themename, explanation FROM theme ORDER BY id ASC");
+            $stmt->execute();
+            $stmt->bind_result($id, $name, $exp);
+            while ($stmt->fetch()) {
+                $name = htmlentities($name);
+                echo "<tr>";
+                    echo "<th scope=\"row\"> $id </th>";
+                    echo "<td> $name </td>";
+                    echo "<td> button goes here </td>";
+                echo "</tr>";
+            }
+        ?>
+        </tbody>
+    </table>
 
+<!-- all the ROLES -->
 
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th scope="col">
-                #
-            </th>
-            <th scope="col">
-                Theme Name
-            </th>
-            <th scope="col">
-                See More
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-        // $conn = new mysqli("localhost", "root", "", "g52grp");
-        // if ($conn->connect_errno != 0) {
-        //     die('Failed to connect to the database.');
-        // }
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">
+                    #
+                </th>
+                <th scope="col">
+                    Role Name
+                </th>
+                <th scope="col">
+                    See More
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            $stmt = $conn->prepare("SELECT id, entry FROM role ORDER BY id ASC");
+            $stmt->execute();
+            $stmt->bind_result($id, $entry);
+            while ($stmt->fetch()) {
+                $entry = htmlentities($entry);
+                echo "<tr>";
+                    echo "<th scope=\"row\"> $id </th>";
+                    echo "<td> $entry </td>";
+                    echo "<td> button goes here </td>";
+                echo "</tr>";
+            }
+        ?>
+        </tbody>
+    </table>
+    
+    <!-- all the ELEMENTS -->
 
-        $stmt = $conn->prepare("SELECT id, themename, explanation FROM theme ORDER BY id ASC");
-        $stmt->execute();
-        $stmt->bind_result($id, $name, $exp);
-        // $counter = 1;
-
-        while ($stmt->fetch()) {
-            $name = htmlentities($name);
-            echo "<tr>";
-            echo "<th scope=\"row\"> $id </th>";
-            echo "<td> $name </td>";
-            echo "<td> button goes here </td>";
-            // echo $name;
-            // echo "<br>";
-        }
-    ?>
-
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">
+                    #
+                </th>
+                <th scope="col">
+                    Role Name
+                </th>
+                <th scope="col">
+                    See More
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            $stmt = $conn->prepare("SELECT id, elementname FROM element ORDER BY id ASC");
+            $stmt->execute();
+            $stmt->bind_result($id, $elename);
+            while ($stmt->fetch()) {
+                $elename = htmlentities($elename);
+                echo "<tr>";
+                    echo "<th scope=\"row\"> $id </th>";
+                    echo "<td> $elename </td>";
+                    echo "<td> button goes here </td>";
+                echo "</tr>";
+            }
+        ?>
+        </tbody>
+    </table>
 </body>
