55b7fd716bce6f9f41df0330af051f62f994b268
alexhambley <hambleyjones@gmail.com>
2018-01-26 14:44:10 +0000

    Navbar
    Navbar php file

---
+++ src/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php 
+include "header.php";
+include "js/footer.js";
+?>
+<body>
+    <nav>            
+        <ul class="nav nav-pills">
+            <li>
+                <a href="#"> Students </a>
+            </li>
+            <li>
+                <a href="allThemes.php"> Public </a>
+            </li>
+            <li>
+                <a href="login"> Admin </a>
+            </li>
+        </ul>
+    </nav>
+</body>
+
+<script>
+// $(document).ready(function() {
+//     $('li.active').removeClass('active');
+//     $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+// });
+</script>
