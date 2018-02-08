d309749130453f11bc547d448939f68ebdce8b0d
alexhambley <hambleyjones@gmail.com>
2018-02-08 11:06:19 +0000

    Dynamic search functionality

---
+++ src/ajax.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<?php
+    include "db.php";
+    if (isset($_POST['search'])) {
+        $searchresult = $_POST['search'];
+        $stmt = $conn->prepare("SELECT element.elementname
+                                FROM element
+                                WHERE lower(element.elementname)
+                                LIKE lower('%$searchresult%')
+                                UNION
+                                SELECT role.entry
+                                FROM role
+                                WHERE lower(role.entry)
+                                LIKE lower('%$searchresult%')
+                                UNION
+                                SELECT theme.themename
+                                FROM theme
+                                WHERE lower(theme.themename)
+                                LIKE lower('%$searchresult%')");
+        // var_dump($stmt);
+        // echo "$stmt";
+        $stmt->execute();
+        // var_dump($stmt->execute());
+
+        $stmt->bind_result($searchresult_show);
+        while ($stmt->fetch()) {
+            $searchresult_show = htmlentities($searchresult_show);
+        }
+        echo "<ul>";
+        echo "<li onclick='fill'";
+        echo "(\"echo $searchresult_show;\")'>";
+        echo "<a>";
+        echo $searchresult_show;
+        echo "</li></a>";
+        echo "</ul>";
+    }
+?>
+<?php
+    // echo $Result['Name'];
+?>
--- src/navbar.php
+++ src/navbar.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <?php include "js/footer.js"; ?>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
 <body>
     <style>
     /* Otherwise the navbar will cover stuff in body */
         body { padding-top: 70px; }
     </style>
-    <nav class="navbar navbar-default navbar-fixed-top">     
+    <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
                 <a class="navbar-brand">Competencies for Food Graduates</a>
@@ -14,14 +16,64 @@
                 <li><a href="view_students.php"> Students </a></li>
                 <li><a href="view_all.php"> Public (View All) </a></li>
                 <li><a href="admin_login.php"> Admin Login </a></li>
+                <li><input type="text" id="search" class="form-control" placeholder="Search"></li>
+                <div id="display"></div>
             </ul>
         </div>
     </nav>
 </body>
 
 <script>
+
+//Getting value from "ajax.php".
+function fill(Value) {
+   //Assigning value to "search" div in "search.php" file.
+   $('#search').val(Value);
+   //Hiding "display" div in "search.php" file.
+   $('#display').hide();
+}
+
+$(document).ready(function() {
+   //On pressing a key on "Search box" in "search.php" file. This function will be called.
+   $("#search").keyup(function() {
+
+       //Assigning search box value to javascript variable named as "name".
+
+       var name = $('#search').val();
+
+       //Validating, if "name" is empty.
+
+       if (name == "") {
+           //Assigning empty value to "display" div in "search.php" file.
+           $("#display").html("");
+       }
+
+       //If name is not empty.
+
+       else {
+           //AJAX is called.
+           $.ajax({
+               //AJAX type is "Post".
+               type: "POST",
+               //Data will be sent to "ajax.php".
+               url: "ajax.php",
+               //Data, that will be sent to "ajax.php".
+               data: {
+                   //Assigning value of "name" into "search" variable.
+                   search: name
+               },
+               //If result found, this funtion will be called.
+               success: function(html) {
+                   //Assigning result to "display" div in "search.php" file.
+                   $("#display").html(html).show();
+                   console.console.log(search);
+               }
+           });
+       }
+   });
+});
 // $(document).ready(function() {
 //     $('li.active').removeClass('active');
-//     $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+//     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 // });
 </script>
