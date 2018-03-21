75392ec01df44626f59262895e9671647bb2ec8c
alexhambley <hambleyjones@gmail.com>
2018-03-21 14:05:37 +0000

    Added navbar to admin page and removed duplicate funcationality

--- src/index_admin.php
+++ src/index_admin.php
@@ -1,4 +1,7 @@
 <?php
+include "header.php";
+include "navbar.php";
+include "db.php";
     session_start();
     if (!$_SESSION['loggedin']) {
         header('Location: login.php');
@@ -13,59 +16,59 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        
+
 
         <link type="text/css" rel="stylesheet" href="css/shared.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
-        
+
         <style type="text/css">
             a {color: #36f; }
             select {font-size: 14px; line-height: 18px; color: royalblue;}
         </style>
-        
-        
-    
+
+
+
     </head>
-        
+
     <body onload="loadAllRoles()">
-        
+
         <div class="mainlayer">
             <h1>Competencies for Food Graduate Careers</h1>
             <p>&nbsp;</p>
-            
-            <h2>1. Database Management</h2>
+
+            <h2>Database Management</h2>
             <p><a href="admin_add.php">Add</a></p>
             <p><a href="admin_update.php">Update</a></p>
             <p><a href="admin_remove.php">Remove</a></p>
             <p>&nbsp;</p>
-            
-            <h2>2. View Role Profile</h2>
+
+            <!-- <h2>2. View Role Profile</h2>
             <p>
                 <select id="role_list" onchange="viewRoleProfile()"></select>
-            </p>
-            
-            <p>&nbsp;</p>
-            
+            </p> -->
+
+            <!-- <p>&nbsp;</p>
+
             <h2>3. Search Roles</h2>
             <p><a href="search.php">Search Roles by themes and elements</a></p>
-            
-            <p>&nbsp;</p>
-            
-            <h2>4. Elements of Themes</h2>
-            <p><a href="allThemes.php">List all themes with their elements</a></p>
 
-            <h2>5. Create an new account</h2>
+            <p>&nbsp;</p>
+
+            <h2>4. Elements of Themes</h2>
+            <p><a href="allThemes.php">List all themes with their elements</a></p> -->
+
+            <h2>Create an new account</h2>
             <p><a href="signup.php">Create!</a></p>
-        </div>    
-        
-        
-        
-        
-        
+        </div>
+
+
+
+
+
         <script src="js/index.js"></script>
     </body>
 </html>
