c2166d77442522c32ae3a37f176e2e254e9695d9
alexhambley <hambleyjones@gmail.com>
2018-04-06 17:05:40 +0100

    adding new user css

--- src/index_admin.php
+++ src/index_admin.php
@@ -13,33 +13,21 @@ include "navbar.php";
   <head>
     <link rel="stylesheet" href="css/index_admin.css" />
     <title>Admin</title>
-    <!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <!-- <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
-    <!-- <link rel="icon" href="/favicon.ico" type="image/x-icon"> -->
   </head>
-
-  <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-
   <body class="bg-grey" onload="loadAllRoles()">
     <div class="bg">
       <div class="container">
-
-      <!-- <div class="container-fluid text-center"> -->
       <div class="text-center">
         <h1> Admin</h1>
       </div>
-          <!-- <h1>Admin</h1> -->
-          <h2>Database Management: </h2>
-          <h4> Here you can add new roles, themes and elements, as well as update and remove existing ones. </h4>
-          <p><a href="admin_add.php" class="btn btn-primary">Add</a></p>
-          <p><a href="admin_update.php" class="btn btn-primary">Update</a></p>
-          <p><a href="admin_remove.php" class="btn btn-primary">Remove</a></p>
+        <h2>Database Management: </h2>
+        <h4> Here you can add new roles, themes and elements, as well as update and remove existing ones. </h4>
+        <p><a href="admin_add.php" class="btn btn-primary">Add</a></p>
+        <p><a href="admin_update.php" class="btn btn-primary">Update</a></p>
+        <p><a href="admin_remove.php" class="btn btn-primary">Remove</a></p>
           <h2>Create a new account:</h2>
           <h4> This account will have admin privileges, and therefore will be able to update and remove elements from the database. </h4>
           <p><a href="signup.php" class="btn btn-primary">Create Account</a></p>
-      <!-- </div> -->
     </div>
   </div>
     <script src="js/index.js"> </script>
--- src/signup.php
+++ src/signup.php
@@ -1,17 +1,17 @@
 <?php
+    include "header.php";
+    include "navbar.php";
     include '_db-user-util.php';
     session_start();
     if (!$_SESSION['loggedin']) {
         header('Location: index.php');
         exit();
     }
-
     $existName = false;
     $existEmail = false;
     $existPhone = false;
     $isValid = false;
     $errorDB = false;
-    
     if (isset($_POST['submit']) and isset($_POST['username']) and isset($_POST['password']) and isset($_POST['email']) and isset($_POST['birthday']) and isset($_POST['phone'])) {
         $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
         $password = md5($_POST['password']);
@@ -38,9 +38,7 @@
             catch (Exception $e) {
                 $errorDB = true;
             }
-
         }
-
         if (!$existName and !$existEmail and !$existPhone and $isValid and !$errorDB) {
             header('Location: index_admin.php');
             exit();
@@ -50,17 +48,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Sign Up</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title> Add New Admin </title>
+    <link rel="stylesheet" href="css/view_students.css" />
 </head>
-<body>
 
+
+<body class="bg-grey">
     <div class="container">
         <div class="text-center">
             <h1>Register Your Account</h1>
         </div>
 
-        <form class="col-md-4 col-md-offset-4" id="signup" action="signup.php" method="POST">
+        <h4> Explanation: </h4>
+        <p> You can add additional administrators to this site. <br>
+            These admins are also able to make changes to the database.
+            This includes adding new roles, themes and elements, as well as updating existing roles, themes and elements. </p>
+
+        <form id="signup" action="signup.php" method="POST">
             <div class="form-group has-error text-center">
             <?php if ($existName) { ?>
                 <p class="help-block">This username already exists!</p>
@@ -75,46 +79,39 @@
                 <p class="help-block">Error Connecting DB! Try again!</p>
             <?php } ?>
             </div>
+
             <div class="form-group" id="div-username">
                 <div class="input-group">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                     <input class="form-control" type="text" name="username" placeholder="Username" required>
                 </div>
-                <span class="help-block hidden" id="username-invalid">Invalid username! (only letters and spaces)</span>
+                <span class="help-block hidden" id="username-invalid">Invalid username! (Letters and spaces only)</span>
             </div>
             <div class="form-group" id="div-password">
                 <div class="input-group">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                     <input class="form-control" type="password" name="password" placeholder="Password" required>
                 </div>
-                <span class="help-block hidden" id="password-invalid">Invalid password! (Min length is 6)</span>
+                <span class="help-block hidden" id="password-invalid">Invalid password! (Minimum length is 6)</span>
             </div>
+
+            <h4> Security Questions: </h4>
             <div class="form-group">
-                <div class="input-group">
-                    
-                </div>
+              <label for "email"> Email Address: </label>
+                <input class="form-control" type="email" name="email" placeholder="name@nottingham.ac.uk" required>
             </div>
+
             <div class="form-group">
-                <div class="input-group">
-                    <h5>What's Your Email Address?</h5>
-                    <input class="form-control" type="text" name="email" placeholder="abc@nottingham.ac.uk">
-                </div>
+              <label for "birthday"> Birthday: </label>
+                <input class="form-control" type="date" name="birthday" placeholder="01/01/1970" required>
+                 <small id="bdayhelp" class="form-text text-muted">Please use the format dd/mm/yyyy</small>
             </div>
+
             <div class="form-group">
-                <div class="input-group">
-                    <h5>What's Your Birthday?</h5>
-                    <input class="form-control" type="text" name="birthday" placeholder="1970-01-01">
-                </div>
+              <label for "number"> Phone Number: </label>
+              <input class="form-control" type="number" name="phone" placeholder="07770000000" required>
             </div>
-            <div class="form-group">
-                <div class="input-group">
-                    <h5>What's Your Phone Number?</h5>
-                    <input class="form-control" type="text" name="phone" placeholder="123456789">
-                </div>
-            </div>
-            <div class="form-group">
-                <input class="form-control btn btn-primary" type="submit" name="submit" value="Register">
-            </div>
+            <input class="btn btn-primary" type="submit" name="submit" style="background-color: #2a8c3e" value="Register New Admin">
         </form>
     </div>
 </body>
