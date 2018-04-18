df1e289f62becbcd25641a093ba258cb7d2f5d24
alexhambley <hambleyjones@gmail.com>
2018-04-18 16:25:57 +0100

    Added wheel to view_students

--- src/view_students.php
+++ src/view_students.php
@@ -12,102 +12,94 @@
 </head>
 
 <body class="bg-grey">
-<!-- Search Bar goes here -->
-
-<!-- Form Starts here
-     Theme 1 -->
-
-<!-- See Trello if confused.
-     The idea is that user chooses three themes, and then we show the
-     elements from these three themes.
-
-     If we show all 40+ elements, that is poor UI design.
-     User chooses 3 different themes, then 10 correspondng elements.
-     We then show the best matching role    s.  -->
-
     <div class="container">
         <form action="view_elements.php">
-
-    
             <div class="text-center">
-                <h1> Find a Career </h1>
+                <h1> Find Ideal Career Pathways for You </h1>
             </div> 
             <h4> Explanation: </h4>
-            <p> All of the elements, or skills, are associated with themes. <br>
-                Here, you can select the three themes that you believe are most appropiate to you, and then submit these choices. <br>
-                You may also wish to select elements from all the themes.  </p>
-            <label for="theme1"> Theme 1 </label>
-            <br>
-            <div class="select">
-            <select class="custom-select" name="theme1" required>
-                <option value="" selected> Please select theme 1 </option>
-                <?php
-                    $stmt = $conn->prepare("SELECT theme_id, themename
-                                            FROM theme
-                                            ORDER BY theme_id ASC");
-                    $stmt->execute();
-                    $stmt->bind_result($id, $name);
-                    while ($stmt->fetch()) {
-                        $id = htmlentities($id);
-                        $name = htmlentities($name);
-                        echo "<option value=\"$id\">$name</option>";
-                    }
-                ?>
-            </select>
-            </div>
-            <br>
-<!-- Theme 2 -->
-            <label for="theme2"> Theme 2 </label>
-            <br>
-            <div class="select">
-            <select class="custom-select" name="theme2" required>
-                <option value="" selected> Please select theme 2 </option>
-                <?php
-                    $stmt = $conn->prepare("SELECT theme_id, themename
-                                            FROM theme
-                                            ORDER BY theme_id ASC");
-                    $stmt->execute();
-                    $stmt->bind_result($id, $name);
-                    while ($stmt->fetch()) {
-                        $id = htmlentities($id);
-                        $name = htmlentities($name);
-                        echo "<option value=\"$id\">$name</option>";
-                    }
-                ?>
-            </select>
-            </div>
-            <br>
+                    <p>There are 8 themes and 48 elements. These elements are associated to one of more of these themes. <br>
+                    Here, you can select up to 3 themes you feel are a personal strength <br>
+                    Alternatively click on ‘Enter Element Choices Only’ to focus on selecting specific elements instead </p>
 
-    <!-- Theme 3 -->
-            <label for="theme3"> Theme 3 </label>
-            <br>
-            <div class="select">
-            <select class="custom-select" name="theme3" required>
-                <option value="" selected>Please select theme 3</option>
-                <?php
-                    $stmt = $conn->prepare("SELECT theme_id, themename
-                                            FROM theme
-                                            ORDER BY theme_id ASC");
-                    $stmt->execute();
-                    $stmt->bind_result($id, $name);
-                    while ($stmt->fetch()) {
-                        $id = htmlentities($id);
-                        $name = htmlentities($name);
-                        echo "<option value=\"$id\">$name</option>";
-                    }
-                ?>
-            </select>
+            <div class="row">
+                <div class="col-sm-8">
+                   
+                    <label for="theme1"> Theme 1 </label>
+                    <br>
+                    <div class="select">
+                    <select class="custom-select" name="theme1" required>
+                        <option value="" selected> Please select theme 1 </option>
+                        <?php
+                            $stmt = $conn->prepare("SELECT theme_id, themename
+                                                    FROM theme
+                                                    ORDER BY theme_id ASC");
+                            $stmt->execute();
+                            $stmt->bind_result($id, $name);
+                            while ($stmt->fetch()) {
+                                $id = htmlentities($id);
+                                $name = htmlentities($name);
+                                echo "<option value=\"$id\">$name</option>";
+                            }
+                        ?>
+                    </select>
+                    </div>
+                    <br>
+                    <label for="theme2"> Theme 2 </label>
+                    <br>
+                    <div class="select">
+                    <select class="custom-select" name="theme2" required>
+                        <option value="" selected> Please select theme 2 </option>
+                        <?php
+                            $stmt = $conn->prepare("SELECT theme_id, themename
+                                                    FROM theme
+                                                    ORDER BY theme_id ASC");
+                            $stmt->execute();
+                            $stmt->bind_result($id, $name);
+                            while ($stmt->fetch()) {
+                                $id = htmlentities($id);
+                                $name = htmlentities($name);
+                                echo "<option value=\"$id\">$name</option>";
+                            }
+                        ?>
+                    </select>
+                    </div>
+                    <br>
+                    <label for="theme3"> Theme 3 </label>
+                    <br>
+                    <div class="select">
+                    <select class="custom-select" name="theme3" required>
+                        <option value="" selected>Please select theme 3</option>
+                        <?php
+                            $stmt = $conn->prepare("SELECT theme_id, themename
+                                                    FROM theme
+                                                    ORDER BY theme_id ASC");
+                            $stmt->execute();
+                            $stmt->bind_result($id, $name);
+                            while ($stmt->fetch()) {
+                                $id = htmlentities($id);
+                                $name = htmlentities($name);
+                                echo "<option value=\"$id\">$name</option>";
+                            }
+                        ?>
+                    </select>
+                    </div>
+                    <br>
+                    <br>
+                </fieldset>
+            </form>
+        </div>
+        <div class="col-sm-4">
+            <div class="wheel-image">
+                <img src="img/wheel.png" alt = "Theme">
             </div>
-            <br>
-            <br>
-            <button class="btn btn-default" type="submit" style="border-color: #192A6C">
-                    Submit Theme Choices
-            </button>
-            <button class="btn btn-default" type="submit" name=skip value="true" style="border-color: #192A6C">
-                    Enter Element Choices Only
-            </button>
-        </fieldset>
-    </form>
+        </div>
     </div>
 
+    <button class="btn btn-default" type="submit" style="border-color: #192A6C">
+        Submit Theme Choices
+    </button>
+    <button class="btn btn-default" type="submit" name=skip value="true" style="border-color: #192A6C">
+        Enter Element Choices Only
+    </button>
 </body>
