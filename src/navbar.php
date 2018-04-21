38b764843c70fe595198d686ec3ba09d864f1b82
alexhambley <hambleyjones@gmail.com>
2018-04-21 14:18:46 +0100

    Navbar works on mobile

--- src/navbar.php
+++ src/navbar.php
@@ -4,76 +4,140 @@
 <head>
     <style type="text/css">
     .navbar-header img {
-        width: 15%;
+       display: block;
+        /* max-width:150px; */
+        /* max-height:auto; */
+        /* width: auto; */
+        /* height: auto; */
+        width: 20%;
         height: auto;
         float:left;
-        text-align: center;
-        vertical-align: middle;
+        text-align: center; 
         padding: 4pt;
     }
-    .nav {
-        line-height:30px;
+    .nav { 
+        line-height:20px;
         float: right;
         vertical-align: middle;
     }
     .btn {
         color: white;
     }
-    .navbar-brand {
-        padding-top: 23pt;
+    .navbar-brand { 
+        /* line-height: 80px; */
+        /* padding-top: 14pt; */
+        /* padding-bottom: 2pt; */
         padding-left: 20pt;
         color: #1c2c67;
     }
     .navbar-nav {
-        padding: 12pt;
+        padding-top: 2pt;
+        padding-bottom: 2pt;
     }
 
     .navbar {
       background-color: white;
-
     }
 
     #nav_bar .active {
     color:            #F8F8F8;
     background-color: #4f81bd;
 }
+
+@media (max-width: 1080px) {
+    .navbar-header {
+        float: none;
+    }
+    .navbar-left,.navbar-right {
+        float: none !important;
+    }
+    .navbar-toggle {
+        display: block;
+    }
+    .navbar-collapse {
+        border-top: 1px solid transparent;
+        box-shadow: inset 0 1px 0 rgba(255,255,255,0.1);
+    }
+    .navbar-fixed-top {
+        top: 0;
+        border-width: 0 0 1px;
+    }
+    .navbar-collapse.collapse {
+        display: none!important;
+    }
+    .navbar-nav {
+        float: none!important;
+        margin-top: 7.5px;
+    }
+    .navbar-nav>li {
+        float: none;
+    }
+    .navbar-nav>li>a {
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+    .collapse.in{
+        display:block !important;
+    }
+    .uon-image {
+        display: none;
+    }
+    .navbar-brand {
+        font-size: 12px;
+    }
+
+}
+
+    
     </style>
 </head>
 <body>
     <style>
     /* Otherwise the navbar will cover stuff in body */
     /* <a href="http://www.nottingham.ac.uk/" style="background-image: url(img/UoN_Primary_Logo_RGB.png); float: left; padding-left: 10px; padding-bottom: 5px"></a>*/
+
         body {
-            padding-top: 70px;
+            padding-top: 50px;
         }
     </style>
+
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a href="http://www.nottingham.ac.uk/"> <img id="image" src="img/UoN_Primary_Logo_RGB.png"> </a>        
-                <a class="navbar-brand" href="index.php" style="color: #1c2c67; font-weight: bold">Competencies for Food Graduate Careers</a>
-                <ul id='navbar' class="nav navbar-nav ">
-                    <!-- <li id="navbar-index"></li> --> 
-                    <li> <a href="index.php" style="color: #1a296b; font-weight: bold"> View All </a></li>
-                    <li> <a href="view_students.php" style="color: #1a296b; font-weight: bold"> Find a Career </a></li>
-                    <li> <a href="admin_login.php"  style="color: #1a296b; font-weight: bold"> Admin </a></li>
-                    <li>
-                        <form id="searchform" class="navbar-form navbar-left" method="get" action="navbar_search.php">
-                            <div class="input-group">
-                                <input type="text" name="selection" id="search" class="form-control" placeholder="Search">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-default" type="submit" form="searchform" style="float:right">
-                                            <i class="glyphicon glyphicon-search"></i>
-                                        </button>
-                                    </div>
-                                </input>
-                            </div>
-                        </form>
-                    </li>
-                </ul>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <div class="uon-image"> 
+                <a href="http://www.nottingham.ac.uk/"> <img id="image" src="img/UoN_Primary_Logo_RGB.png"> </a> 
             </div>
+            <a class="navbar-brand" href="index.php" style="color: #1c2c67; font-weight: bold">Competencies for Food Graduate Careers</a> 
+
         </div>
-    </nav>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav"> 
+                <li> <a href="index.php" style="color: #1a296b; font-weight: bold"> View All </a></li>
+                <li> <a href="view_students.php" style="color: #1a296b; font-weight: bold"> Find a Career </a></li>
+                <li> <a href="admin_login.php"  style="color: #1a296b; font-weight: bold"> Admin </a></li>
+           
+        
+                <li>
+                    <form id="searchform" class="navbar-form navbar-left form-inline my-2 my-lg-0" method="get" action="navbar_search.php">
+                        <div class="input-group">
+                            <input type="text" name="selection" id="search" class="form-control mr-sm-2" placeholder="Search">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-default my-2 my-sm-0"type="submit" form="searchform" style="float:right">
+                                        <i class="glyphicon glyphicon-search"></i>
+                                    </button>
+                                </div>
+                            </input>
+                        </div>
+                    </form>
+                </li>
+            </ul> 
+        </div>
+    </nav> 
 </body>
 
 
